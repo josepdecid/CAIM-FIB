7b98527f1ef19174f780b76dc29cfff2037346fb
josep.de.cid <josep.de.cid@fib.upc.edu>
2018-11-02 02:34:12 +0100

    Add new S3

--- S3/Rocchio.py
+++ S3/Rocchio.py
@@ -10,10 +10,10 @@ from .search_index_weights import execute_query
 class Rocchio:
     def __init__(self):
         self.alpha = 1
-        self.beta = 1
-        self.gamma = 1
-        self.nrounds = 10
+        self.beta = 10
+        self.n_rounds = 10
         self.k = 5
+        self.r = 5
 
     @staticmethod
     def __add_term_vectors(x, y):
@@ -51,13 +51,11 @@ class Rocchio:
             s = word.split('^')
             return (word, 1) if len(s) == 1 else (s[0], float(s[1]))
 
-        query = list(map(set_term_weights, query.split()))
-        return Rocchio.__normalize(query)
+        return list(map(set_term_weights, query))
 
     @staticmethod
     def __term_vector_to_query(tw):
-        return reduce(lambda acc, x:
-                      acc + ' ' + x[0] + '^' + str(round(x[1])) if round(x[1]) != 0 else acc, tw, '')[1:]
+        return list(map(lambda x: x[0] + '^' + str(x[1]), tw))
 
     @staticmethod
     def __normalize(tw):
@@ -90,33 +88,72 @@ class Rocchio:
         vector = list(map(tf_idf, zip(term_frequency, document_appearances)))
         return Rocchio.__normalize(vector)
 
-    def update_parameters(self, alpha, beta, gamma, nrounds, k):
+    def __prune_query(self, q):
+        p = q[0:self.r]
+        p.sort(key=lambda el: el[1])
+        for i in range(self.r, len(q)):
+            for j in range(0, self.r):
+                if q[i][1] > p[j][1]:
+                    p.insert(j, q[i])
+                    break
+        return p[0:self.r]
+
+    @staticmethod
+    def __store_results(query, documents):
+        return {
+            'query': query,
+            'documents': list(map(lambda d: {
+                'path': d.path.split('\\')[1],
+                'score': d.meta.score
+            }, documents))
+        }
+
+    def update_parameters(self, alpha, beta, n_rounds, k, r):
         self.alpha = self.alpha if alpha is None else alpha
         self.beta = self.beta if beta is None else beta
-        self.gamma = self.gamma if gamma is None else gamma
-        self.nrounds = self.nrounds if nrounds is None else nrounds
+        self.n_rounds = self.n_rounds if n_rounds is None else n_rounds
         self.k = self.k if k is None else k
+        self.r = self.r if k is None else r
 
     def query(self, index, query):
+        result = []
+        documents = None
+
         q = Rocchio.__query_to_term_vector(query)
+        q.sort(key=lambda el: el[0])
         client = Elasticsearch()
 
-        for it in range(self.nrounds):
-            print('Round #{}: {}'.format(it, query))
+        for it in range(self.n_rounds):
+            docs = execute_query(index, query, self.k)
+            if len(docs) == 0:
+                if documents is None:
+                    print('No documents found!')
+                    return
+                else:
+                    break
+            else:
+                documents = docs
 
-            documents = execute_query(index, query, self.k)
+            result.append(Rocchio.__store_results(query, documents))
             relevant_term_vectors = list(map(lambda doc: Rocchio.__to_tf_idf(client, index, doc.meta.id), documents))
 
+            # alpha * query
             terms, weights = zip(*q)
             weights = (self.alpha * np.array(weights)).tolist()
             old = list(zip(terms, weights))
 
+            # beta * sum(docs)
             sum_relevant_documents = reduce(Rocchio.__add_term_vectors, relevant_term_vectors)
             terms, weights = zip(*sum_relevant_documents)
             weights = (self.beta * np.array(weights) / len(sum_relevant_documents)).tolist()
             relevant = list(zip(terms, weights))
 
             q = Rocchio.__add_term_vectors(old, relevant)
+            q = self.__prune_query(q)
+            q = Rocchio.__normalize(q)
+            q.sort(key=lambda el: el[0])
             query = Rocchio.__term_vector_to_query(q)
 
         documents = execute_query(index, query, self.k)
+        result.append(Rocchio.__store_results(query, documents))
+        return result
--- S3/main.py
+++ S3/main.py
@@ -1,5 +1,40 @@
+import json
+
+import numpy as np
+
 from S3.Rocchio import Rocchio
+from sklearn.model_selection import ParameterGrid
 
 if __name__ == '__main__':
-    r = Rocchio()
-    r.query('news', 'toronto')
+    rocchio = Rocchio()
+
+    param_grid = {
+        'alpha': np.arange(1, 50, 2),
+        'beta': np.arange(1, 50, 2),
+        'n_rounds': np.arange(1, 100, 10),
+        'k': np.arange(5, 50, 5),
+        'r': np.arange(2, 11)
+    }
+
+    params = ParameterGrid(param_grid)
+
+    experiments = []
+    for p in params:
+        rocchio.update_parameters(alpha=p['alpha'],
+                                  beta=p['beta'],
+                                  n_rounds=p['n_rounds'],
+                                  k=int(p['k']),
+                                  r=p['r'])
+        results = rocchio.query('news', ['toronto', 'vancouv^2'])
+        experiments.append({
+            'parameters': {
+                'alpha': int(p['alpha']),
+                'beta': int(p['beta']),
+                'n_rounds': int(p['n_rounds']),
+                'k': int(p['k']),
+                'r': int(p['r']),
+            }, 'results': results
+        })
+
+    with open('results.json', mode='w') as f:
+        f.write(json.dumps(experiments))
