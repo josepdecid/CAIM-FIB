1804e00f59e723bd00581bc8e30551230f8f110f
JosepRIvaille <josep.de.cid@gmail.com>
2018-11-11 20:54:15 +0100

    Move read to airport class

--- S5/Airport.py
+++ S5/Airport.py
@@ -1,10 +1,38 @@
+import re
+
+
 class Airport:
     def __init__(self, iata=None, name=None):
         self.code = iata
         self.name = name
         self.routes = []
-        self.routeHash = dict()
-        self.outweight = 1 # write appropriate value
+        self.route_hash = dict()
+        self.out_weight = 1  # write appropriate value
 
     def __repr__(self):
         return "{0}\t{2}\t{1}".format(self.code, self.name, self.pageIndex)
+
+    @staticmethod
+    def read_airports(path):
+        airport_list = []  # list of Airport
+        airport_hash = {}  # hash key IATA code -> Airport
+
+        print('Reading Airport file from {0}'.format(path))
+        with open(path, mode='r', encoding='utf8') as f:
+            cont = 0
+            for line in f.readlines():
+                s = line.split(',')
+                if not re.match('"[A-Z]{3}"', s[4]):
+                    continue  # Invalid or missing IATA code
+
+                a = Airport()
+                a.name = s[1][1:-1] + ", " + s[3][1:-1]
+                a.code = s[4][1:-1]
+
+                airport_list.append(a)
+                airport_hash[a.code] = a
+
+                cont += 1
+        print('There were {0} Airports with IATA code'.format(cont))
+
+        return airport_list, airport_hash
--- S5/PageRank.py
+++ S5/PageRank.py
@@ -1,10 +1,5 @@
 from __future__ import print_function
 
-import re
-from collections import namedtuple
-import time
-import sys
-from Edge import Edge
 from Airport import Airport
 
 AIRPORTS_FILE = 'airports_filtered.txt'
@@ -13,28 +8,6 @@ ROUTES_FILE = 'routes.txt'
 
 edgeList = []  # list of Edge
 edgeHash = dict()  # hash of edge to ease the match
-airportList = []  # list of Airport
-airportHash = dict()  # hash key IATA code -> Airport
-
-
-def read_airports(path):
-    print('Reading Airport file from {0}'.format(path))
-    with open(path, mode='r', encoding='utf8') as f:
-        cont = 0
-        for line in f.readlines():
-            s = line.split(',')
-            if not re.match('"[A-Z]{3}"', s[4]):
-                continue # Invalid or missing IATA code
-
-            a = Airport()
-            a.name = s[1][1:-1] + ", " + s[3][1:-1]
-            a.code = s[4][1:-1]
-
-            airportList.append(a)
-            airportHash[a.code] = a
-
-            cont += 1
-    print('There were {0} Airports with IATA code'.format(cont))
 
 
 def readRoutes(fd):
@@ -53,7 +26,7 @@ def outputPageRanks():
 
 
 if __name__ == '__main__':
-    readAirports(AIRPORTS_FILE)
+    airports_list, airports_hash = Airport.read_airports(AIRPORTS_FILE)
     readRoutes(ROUTES_FILE)
     # time1 = time.time()
     # iterations = computePageRanks()
