3fc02d992abec7b28f27e65ef213c057971fd116
adrian.sanchez.albanell <adrian.sanchez.albanell@fib.upc.edu>
2018-11-11 20:30:51 +0100

    Edge and UTF8

---
+++ S5/Edge.py
@@ -0,0 +1,9 @@
+class Edge:
+    def __init__(self, origin=None):
+        self.origin = ...  # write appropriate value
+        self.weight = ...  # write appropriate value
+
+    def __repr__(self):
+        return "edge: {0} {1}".format(self.origin, self.weight)
+
+    # write rest of code that you need for this class
--- S5/PageRank.py
+++ S5/PageRank.py
@@ -4,45 +4,40 @@ from __future__ import print_function
 from collections import namedtuple
 import time
 import sys
+from Edge import Edge
+from Airport import Airport
 
-class Edge:
-    def __init__ (self, origin=None):
-        self.origin = ... # write appropriate value
-        self.weight = ... # write appropriate value
-
-    def __repr__(self):
-        return "edge: {0} {1}".format(self.origin, self.weight)
-        
-    ## write rest of code that you need for this class
 
 class Airport:
-    def __init__ (self, iden=None, name=None):
+    def __init__(self, iden=None, name=None):
         self.code = iden
         self.name = name
         self.routes = []
         self.routeHash = dict()
-        self.outweight =    # write appropriate value
+        self.outweight = ...  # write appropriate value
 
     def __repr__(self):
         return "{0}\t{2}\t{1}".format(self.code, self.name, self.pageIndex)
 
-edgeList = [] # list of Edge
-edgeHash = dict() # hash of edge to ease the match
-airportList = [] # list of Airport
-airportHash = dict() # hash key IATA code -> Airport
+
+edgeList = []  # list of Edge
+edgeHash = dict()  # hash of edge to ease the match
+airportList = []  # list of Airport
+airportHash = dict()  # hash key IATA code -> Airport
+
 
 def readAirports(fd):
     print("Reading Airport file from {0}".format(fd))
-    airportsTxt = open(fd, "r");
+    airportsTxt = open(fd, "r", encoding="utf8")
     cont = 0
     for line in airportsTxt.readlines():
         a = Airport()
         try:
             temp = line.split(',')
-            if len(temp[4]) != 5 :
+            if len(temp[4]) != 5:
                 raise Exception('not an IATA code')
-            a.name=temp[1][1:-1] + ", " + temp[3][1:-1]
-            a.code=temp[4][1:-1]
+            a.name = temp[1][1:-1] + ", " + temp[3][1:-1]
+            a.code = temp[4][1:-1]
         except Exception as inst:
             pass
         else:
@@ -57,21 +52,26 @@ def readRoutes(fd):
     print("Reading Routes file from {0}".format(fd))
     # write your code
 
+
 def computePageRanks():
+    pass
     # write your code
 
+
 def outputPageRanks():
+    pass
     # write your code
+
 
 def main(argv=None):
     readAirports("airports.txt")
     readRoutes("routes.txt")
-    time1 = time.time()
-    iterations = computePageRanks()
-    time2 = time.time()
-    outputPageRanks()
-    print("#Iterations:", iterations)
-    print("Time of computePageRanks():", time2-time1)
+    # time1 = time.time()
+    # iterations = computePageRanks()
+    # time2 = time.time()
+    # outputPageRanks()
+    # print("#Iterations:", iterations)
+    # print("Time of computePageRanks():", time2-time1)
 
 
 if __name__ == "__main__":
