42001b64ba97b44eb645bdd9867712df787e6380
JosepRIvaille <josep.de.cid@gmail.com>
2018-11-12 23:00:26 +0100

    Add index to airports to match array indices

--- S5/Airport.py
+++ S5/Airport.py
@@ -1,40 +1,38 @@
 import re
-from random import random
 from typing import Dict
 
 
 class Airport:
-    def __init__(self, iata=None, name=None):
+    def __init__(self, index=0, iata=None, name=None):
+        self.index = index
         self.code = iata
         self.name = name
         self.routes = []
         self.route_hash = {}
         self.out_weight = 1  # write appropriate value
-        self.rank = random()
 
     def __repr__(self):
         return "{0}\t{1}".format(self.code, self.name)
 
     @staticmethod
     def read_airports(path):
-        airport_hash: Dict[str, Airport] = {}  # hash key IATA code -> Airport
+        airports_hash: Dict[str, Airport] = {}  # hash key IATA code -> Airport
+        count = 0
 
         print('Reading Airport file from {0}'.format(path))
         with open(path, mode='r', encoding='utf8') as f:
-            cont = 0
             for line in f.readlines():
                 s = line.split(',')
                 if not re.match('"[A-Z]{3}"', s[4]):
-                    continue  # Invalid or missing IATA code
+                    continue  # Invalid or missing IATA code (omit airport)
 
-                a = Airport()
-                a.index = cont
-                a.name = s[1][1:-1] + ', ' + s[3][1:-1]
-                a.code = s[4][1:-1]
+                name = s[1][1:-1] + ', ' + s[3][1:-1]
+                iata_code = s[4][1:-1]
+                a = Airport(index=count, name=name, iata=iata_code)
 
-                airport_hash[a.code] = a
+                airports_hash[a.code] = a
 
-                cont += 1
-        print('There were {0} Airports with IATA code'.format(cont))
+                count += 1
+        print('There were {0} Airports with IATA code'.format(count))
 
-        return airport_hash
+        return airports_hash
